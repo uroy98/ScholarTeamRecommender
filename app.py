eb0f9df507eca6794278a2620ef4b5f591e90d6a
uroy98 <u.roy@mail.missouri.edu>
2026-04-05 02:43:11 -0500

    UI starter: removing large data from history

--- app.py
+++ app.py
@@ -303,4 +303,5 @@ def api_recommend():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True) #for running locally at http://127.0.0.1:5000
+    app.run(host="0.0.0.0", port=5000, debug=False) #For hosting in aws
